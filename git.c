8336907c2ac987fdecc9e9b5e5fd93560459685b
adriacabeza <adriacabeza@hotmail.com>
2019-03-31 22:39:14 +0200

    hints git

---
+++ git.c
@@ -0,0 +1,15 @@
+git add . (o el nom del fitxer)
+git commit -m "nom commit"
+git push 
+
+si vull pillar canvis que estaven al github
+git pull
+
+si hi ha merge conflicts
+git add / git remove (buscar si es rm) dels arxius amb problems
+git commit
+i podre fer pull
+
+
+si me la pela la vida i sta passant conflict
+git stash
